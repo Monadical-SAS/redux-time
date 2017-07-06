24c8cbe80b482af49ec7cb62326194afe27ee8a2
Nick Sweeting <git@nicksweeting.com>
2017-07-06 08:17:21 -0500

    add all animations at warped time instead of actual for easier debugging

--- examples/test-component.js
+++ examples/test-component.js
@@ -13,23 +13,27 @@ const base_style = {
     backgroundColor: 'orange',
 }
 
-const AnimationTesterComponent = ({text, style, animateFirstState, animateSecondState, animateGreen, animateRed, animateMove, animateRotate, animatePulse, animateBlink, animateBlinkStop, clearAnimations, debug}) =>
+const source_tag = <small style={{opacity: 0.2, position: 'absolute', top: -15, right: 5}}>
+    <a href="https://github.com/Monadical-SAS/redux-time/blob/master/examples/test-component.js">examples/test-component.js</a>
+</small>
+
+const AnimationTesterComponent = ({getTime, text, style, animateFirstState, animateSecondState, animateGreen, animateRed, animateMove, animateRotate, animatePulse, animateBlink, animateBlinkStop, clearAnimations, debug}) =>
     <div style={{position: 'relative'}}>
-        {debug ? <small style={{opacity: 0.2, position: 'absolute', top: -15, right: 5}}>examples/test-component.js</small>: null}
+        {debug ? source_tag : null}
         <pre style={{...base_style, ...style}}>
             <br/>
             {text}
             <br/><br/>
         </pre><br/>
-        <Button title="Instant state change" onClick={animateFirstState}>1st state</Button>
-        <Button title="Instant state change" onClick={animateSecondState}>2nd state</Button>
-        <Button title="Instant state change" onClick={animateGreen}>Green</Button>
-        <Button title="Instant state change" onClick={animateRed}>Red</Button>
-        <Button title="Instant state change" onClick={animateMove}>Move</Button>
-        <Button title="Javascript animation" onClick={animateRotate}>Rotate 3x</Button>
-        <Button title="Javascript animation" onClick={animatePulse}>Pulse</Button>
-        <Button title="CSS keyframe animation" onClick={animateBlink}>Blink Forever</Button>
-        <Button title="CSS keyframe animation" onClick={animateBlinkStop}>Stop Blinking</Button>
+        <Button title="Instant state change" onClick={animateFirstState.bind(this, getTime())}>1st state</Button>
+        <Button title="Instant state change" onClick={animateSecondState.bind(this, getTime())}>2nd state</Button>
+        <Button title="Instant state change" onClick={animateGreen.bind(this, getTime())}>Green</Button>
+        <Button title="Instant state change" onClick={animateRed.bind(this, getTime())}>Red</Button>
+        <Button title="Instant state change" onClick={animateMove.bind(this, getTime())}>Move</Button>
+        <Button title="Javascript animation" onClick={animateRotate.bind(this, getTime())}>Rotate 3x</Button>
+        <Button title="Javascript animation" onClick={animatePulse.bind(this, getTime())}>Pulse</Button>
+        <Button title="CSS keyframe animation" onClick={animateBlink.bind(this, getTime())}>Blink Forever</Button>
+        <Button title="CSS keyframe animation" onClick={animateBlinkStop.bind(this, getTime())}>Stop Blinking</Button>
         <Button onClick={clearAnimations}>Reset</Button>
     </div>
 
@@ -43,45 +47,45 @@ const mapDispatchToProps = (dispatch) => ({
     clearAnimations: () => {
         dispatch({type: 'CLEAR_ANIMATIONS'})
     },
-    animateFirstState: () => {
+    animateFirstState: (start_time) => {
         dispatch({type: 'ADD_ANIMATION', animation: Become({
             path: '/test_state/text',
             state: '1st state',
-            start_time: window.animations.time.getWarpedTime(),
+            start_time,
         })})
     },
-    animateSecondState: () => {
+    animateSecondState: (start_time) => {
         dispatch({type: 'ADD_ANIMATION', animation: Become({
             path: '/test_state/text',
             state: '2nd state',
-            start_time: window.animations.time.getWarpedTime(),
+            start_time,
         })})
     },
-    animateGreen: () => {
+    animateGreen: (start_time) => {
         dispatch({type: 'ADD_ANIMATION', animation: Become({
             path: '/test_state/style/color',
             state: 'green',
-            start_time: window.animations.time.getWarpedTime(),
+            start_time,
         })})
     },
-    animateRed: () => {
+    animateRed: (start_time) => {
         dispatch({type: 'ADD_ANIMATION', animation: Become({
             path: '/test_state/style/color',
             state: 'red',
-            start_time: window.animations.time.getWarpedTime(),
+            start_time,
         })})
     },
-    animateMove: () => {
+    animateMove: (start_time) => {
         dispatch({type: 'ADD_ANIMATION', animation: Translate({
             path: '/test_state',
             start_state: {top: 0, left: 0},
             end_state: {top: 100, left: 100},
             duration: 3000,
             unit: 'px',
-            start_time: window.animations.time.getWarpedTime(),
+            start_time,
         })})
     },
-    animateRotate: () => {
+    animateRotate: (start_time) => {
         dispatch({type: 'ADD_ANIMATION', animation: Repeat(Animate({
             path: '/test_state/style/transform/rotate',
             start_state: 0,
@@ -89,30 +93,30 @@ const mapDispatchToProps = (dispatch) => ({
             duration: 3000,
             curve: 'linear',
             unit: 'deg',
-            start_time: window.animations.time.getWarpedTime(),
+            start_time,
         }), 3)})
     },
-    animatePulse: () => {
+    animatePulse: (start_time) => {
         dispatch({type: 'ADD_ANIMATION', animation: AnimateCSS({
             path: '/test_state',
             name: 'pulse',
             duration: 2000,
-            start_time: window.animations.time.getWarpedTime(),
+            start_time,
         })})
     },
-    animateBlink: () => {
+    animateBlink: (start_time) => {
         dispatch({type: 'ADD_ANIMATION', animation: Repeat(AnimateCSS({
             path: '/test_state',
             name: 'blinker',
             duration: 1000,
-            start_time: window.animations.time.getWarpedTime(),
+            start_time,
         }), Infinity)})
     },
-    animateBlinkStop: () => {
+    animateBlinkStop: (start_time) => {
         dispatch({type: 'ADD_ANIMATION', animation: Become({
             path: '/test_state/style/animation/blinker',
             state: null,
-            start_time: window.animations.time.getWarpedTime(),
+            start_time,
         })})
     },
 })
